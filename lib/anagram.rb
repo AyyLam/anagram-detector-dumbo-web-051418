e73b87f07ec1a128751df31d5818dc605c1ba2c0
Anthony Lam <al3101@nyu.edu>
2018-05-20 00:22:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,12 +8,8 @@ class Anagram
   end 
   
   def match(anagrams)
-    matches = [] 
-    anagrams.each do |anagram|
-      if anagram.split("").sort == @word.split("").sort
-        matches << anagram 
+    anagrams.select do |anagram|
+      anagram.split("").sort == @word.split("").sort
       end 
-    end
-    matches
   end 
 end
